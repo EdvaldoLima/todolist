1cece3c77cb03ce54b602bdabf1f07bcac9a9f85
Edvaldo Lima <edvaldojunodi@hotmail.com>
2022-06-09 22:31:25 -0300

    feat: vuex structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <TodoView></TodoView>
+    <TodoView />
   </div>
 </template>
 
@@ -19,12 +19,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Tasks from '@/store/modules/Tasks'
 
 Vue.use(Vuex)
 
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    Tasks
   }
 })
